93ce5d630fc3d8fbb6fd02190786f62cc6fc718f
alebaviera <baviera.alessandra@gmailcom>
2019-08-01 14:28:10 +0200

    creo data dinamica imposto funzione da rivedere ciclo slide

--- main.js
+++ main.js
@@ -7,61 +7,83 @@ $(document).ready(function(){
   var template = Handlebars.compile(source);
 
 
-  var database = moment('2018-03-01')
-  var mesetitle = database.format('MMMM')
-  var mese = parseInt(database.format('M'))-1;
-  var anno = database.format('YYYY')
-  var num = database.daysInMonth();
 
 
-  for (var i = 1; i <= num; i++) {
+  var i = 1;
+  while ( i <= 8) {
+    $('.next').click( function(){
 
+        meseincorso = '0' + String(i);
+        console.log(meseincorso);
+        database = moment('2018-'+ meseincorso + '-01');
+        console.log(database);
+        showMounth('2018-'+ meseincorso + '-01');
 
-    var jan = moment([anno, mese, i]).format('dddd, DD');
-    //console.log(jan);
-    var context = {giorno: jan};
-    var html    = template(context);
-
-
-    $('.calendar').append(html);
-
-
+    });
+  i++;
   }
-  $('.title').text(mesetitle + ' ' + anno);
- // faccio la chiamata ajax
 
 
-  $.ajax({
-    url : feste,
-    data :{year : '2018', month : mese},
-    method : 'GET',
-    success : function(data){
-      var holidays = data.response;
-      for (var i = 0; i < holidays.length; i++) {
-      $('.day').each(function(){
+ function showMounth(stringaargomento){
 
-        var newholiday = moment(holidays[i].date).format('dddd, DD');
+    var database = moment(stringaargomento);
+    var mesetitle = database.format('MMMM');
+    var mese = parseInt(database.format('M'))-1;
+    var anno = database.format('YYYY');
+    var num = database.daysInMonth();
 
-        var data = $(this).html();
-        // console.log(holidays[i]);
 
-        if( data == newholiday){
-          // console.log('sono nellif');
-          $(this).append(' ' + holidays[i].name);
-          $(this).addClass('red');
+    for (var i = 1; i <= num; i++) {
+
+
+      var pagmese = moment([anno, mese, i]).format('dddd, DD');
+      //console.log(pagmese);
+      var context = {giorno: pagmese};
+      var html    = template(context);
+
+
+      $('.calendar').append(html);
+
+
+    }
+    $('.title').text(mesetitle + ' ' + anno);
+   // faccio la chiamata ajax
+
+
+    $.ajax({
+      url : feste,
+      data :{year : '2018', month : mese},
+      method : 'GET',
+      success : function(data){
+        var holidays = data.response;
+        for (var i = 0; i < holidays.length; i++) {
+        $('.day').each(function(){
+
+          var newholiday = moment(holidays[i].date).format('dddd, DD');
+
+          var data = $(this).html();
+          // console.log(holidays[i]);
+
+          if( data == newholiday){
+            // console.log('sono nellif');
+            $(this).append(' ' + holidays[i].name);
+            $(this).addClass('red');
+
+          }
+
+        });
 
         }
 
-      });
+      },
+      error : function(){
 
       }
 
-    },
-    error : function(){
 
-    }
+    });
+  }
 
 
-  });
 
 });
